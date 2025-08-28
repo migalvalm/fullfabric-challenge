c382082e7850491126f2f6ce1ceb739ac2a55403
miguelalvalm <miguelalvalmeida@gmail.com>
2025-08-28 12:13:22 +0100

    refactor to unsorted approach

--- lib/sequence_analyzer.rb
+++ lib/sequence_analyzer.rb
@@ -1,17 +1,18 @@
 module SequenceAnalyzer
-  def self.find_missing_number(arr, nth: 1)    
+  def self.find_missing_number(arr, nth: 1)
+    raise ArgumentError, 'Array must contain unique integers' unless arr.uniq.size == arr.size
+    raise ArgumentError, 'nth must be an integer' unless nth.integer?
+
     return 1 if arr.empty?
-    
-    sorted_arr = arr.sort
+
     current_number = 1
-    missing_count = 0
-    
-    while missing_count < nth
-      if !sorted_arr.include?(current_number)
-        missing_count += 1
-        return current_number if missing_count == nth
-      end
+    missing_number_count = 0
+
+    while missing_number_count < nth
+      missing_number_count += 1 if !arr.include?(current_number)
       current_number += 1
     end
+
+    return current_number
   end
 end
